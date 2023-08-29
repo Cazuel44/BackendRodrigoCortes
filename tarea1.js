dc6f490549731fa5ed5bec720048c1db5ea40e74
Rodrigo Cortes <rorrosb19@gmail.com>
2023-08-29 16:05:44 -0400

    Implementado udpateproduct y deleteproduct ambos por id

--- tarea1.js
+++ tarea1.js
@@ -6,10 +6,11 @@ class ProductManager {
     constructor(){
         this.productos = [];
         this.getProducts();
+        this.idCounter = 0
     }
 
 
-    static id = 0
+    
 
     addProduct(title, description, price, thumbnail, code, stock) {
 
@@ -22,11 +23,11 @@ class ProductManager {
         if(existingProductFilter) {
             console.log("Ya existe un producto con el mismo codigo")
             return;
-        }
+        } 
 
-        ProductManager.id++;
+        this.idCounter++;
         const newProduct = {
-            id: ProductManager.id,
+            id: this.idCounter,
             title,
             description,
             price,
@@ -53,6 +54,8 @@ class ProductManager {
         }
     }
 
+        
+
     getProductsById(id){
         const idFilter = this.productos.find(producto => producto.id === id)
         if(idFilter){
@@ -73,6 +76,37 @@ class ProductManager {
             console.log("Error al cargar productos", error);
         }
     }
+
+    updateProductById = async(id, updateProduct)=>{
+        try {
+            const datos = await fs.promises.readFile("productos.txt", "utf-8");
+            const productosParseados = JSON.parse(datos);
+            const indiceProducto = productosParseados.findIndex(producto => producto.id === id);
+
+            if(indiceProducto != -1){
+                productosParseados[indiceProducto] = {...productosParseados[indiceProducto], ...updateProduct};
+                await fs.promises.writeFile("productos.txt", JSON.stringify(productosParseados), "utf-8")
+                console.log(`producto con id ${id} actualizado`)
+            } else {
+                console.log("producto no encontrado")
+            }
+        } catch (error) {
+            console.log("error maximo")
+        }
+    }
+    
+    deleteProductById = async(id)=>{
+
+        try {
+            const datos = await fs.promises.readFile("productos.txt", "utf-8");
+            const productosParseados = JSON.parse(datos);
+            const productFilter = productosParseados.filter(products => products.id != id)           
+            await fs.promises.writeFile("productos.txt", JSON.stringify(productFilter), "utf-8");
+            console.log("producto eliminado")
+        } catch(error) {
+            console.log("error al leer el archivo", error)
+        }
+    }
 }
 
 const productManager = new ProductManager();
@@ -81,9 +115,15 @@ productManager.addProduct("polera", "talla L", 1500, "./poleraimg.jpg", "codigo1
 productManager.addProduct("pantalon", "talla XL", 1000, "./pantalonimg.jpg", "codigo2", 10)
 productManager.addProduct("zapatilla", "talla 41", 10000, "./zapatillaimg.jpg", "codigo3", )
 productManager.addProduct("poleron", "talla XL", 8500, "./poleronimg.jpg", "codigo1", 15)
+productManager.addProduct("poleron", "talla L", 10000, "./poleron2img.jpg", "codigo4", 7)
 
 productManager.guardarProductosEnArchivoNuevo()
 console.log("Lista de productos:", productManager.productos);
 productManager.getProductsById(1);
 productManager.getProductsById(3);
+productManager.getProductsById(4);
+
+//productManager.deleteProductById(3);
+
+//productManager.updateProductById(1, {price: 3500, description: "talla XL", stock: 5})
 
